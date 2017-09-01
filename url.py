1565ced599a4e126ac6b5063d2f2976bba7e49cf
winecat <winecat@qq.com>
2017-09-01 11:14:06 +0800

    批量请求注册无限别名邮箱 脚本

---
+++ url.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import urllib
+import httplib 
+import sys
+reload(sys)
+print sys.setdefaultencoding('utf-8')
+sys.path.insert(0, 'E:\ProgramData\Anaconda2\Lib\site-packages') ## 本地加载 click模块，正常安装不需要这样子配置
+import requests, json
+
+def do_url_request(numStr):
+    payload = json.dumps({'fullName': 'x' + numStr, 'mail': 'x' + numStr + '@x-mars.cc'}) # 此处将POST的数据定义为一个字典
+    print(payload)
+    # Headers属性初始化
+    headers = {
+        'Host': 'tinypng.com',
+        'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+        'Referer' : 'https://tinypng.com/developers',
+        'Content-Type': 'application/json',
+        'Connection' : 'Keep-Alive'
+        }
+    request_url = 'https://tinypng.com/web/subscription'               # 需要请求的URL地址
+    res = requests.post(request_url, data=payload, headers=headers)
+    print(res.text)
+
+def loop_reqest(start, end):
+    tmp = start
+    while tmp < end:
+        tmpStr = str(tmp)
+        do_url_request(tmpStr)
+        tmp = tmp + 1
+    print "succeed !!! loop request %d --> %d  end"%(start, end)
+    
+
+if __name__ == "__main__":
+    loop_reqest(11, 15)
+    
